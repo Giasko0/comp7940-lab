fbad028b667522a5efe01873d493f6308c409eb9
Giasko0 <dibiame@hotmail.it>
2026-01-22 12:41:59 +0800

    exercises

--- hello.py
+++ hello.py
@@ -1,5 +1,17 @@
 def main():
-    print("Hello World")
+	l = [52633, 8137, 1024, 999]
 
+	for num in l:
+		print_factor(num)	
+
+def print_factor(x):
+	factors = []
+
+	for i in range(2, x//2): # I'm using //2 because no factor will be larger than half of x
+		if not (x % i): # in python anything other than 0 is true
+			factors.append(i)
+
+	print("Factors of ", x, ": ", factors, sep='') if factors else print(x, "is prime.")
+    
 if __name__ == '__main__':
-    main()
+	main()
